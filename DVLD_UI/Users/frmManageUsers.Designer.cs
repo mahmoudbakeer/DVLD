00ae27c77e34fc4e81a8df404013b8369c7cb712
Mahmoud Bakir <mahmoodbaker100@gmail.com>
2026-02-25 23:41:33 +0530

    Update Manage Users form layout

--- DVLD_UI/Users/frmManageUsers.Designer.cs
+++ DVLD_UI/Users/frmManageUsers.Designer.cs
@@ -56,6 +56,7 @@
             this.txtFilter.Name = "txtFilter";
             this.txtFilter.Size = new System.Drawing.Size(185, 20);
             this.txtFilter.TabIndex = 13;
+            this.txtFilter.TextChanged += new System.EventHandler(this.txtFilter_TextChanged);
             // 
             // SortTitle
             // 
@@ -76,7 +77,7 @@
             "None",
             "User ID",
             "Person ID",
-            "Full name",
+            "Full Name",
             "Is Active"});
             this.cbSortBy.Location = new System.Drawing.Point(115, 232);
             this.cbSortBy.Name = "cbSortBy";
@@ -88,7 +89,7 @@
             this.btnAddNewUser.BackColor = System.Drawing.Color.SandyBrown;
             this.btnAddNewUser.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnAddNewUser.ForeColor = System.Drawing.Color.Black;
-            this.btnAddNewUser.Location = new System.Drawing.Point(802, 201);
+            this.btnAddNewUser.Location = new System.Drawing.Point(784, 200);
             this.btnAddNewUser.Name = "btnAddNewUser";
             this.btnAddNewUser.Size = new System.Drawing.Size(129, 53);
             this.btnAddNewUser.TabIndex = 10;
@@ -103,10 +104,11 @@
             this.dgvAllUsers.AllowUserToOrderColumns = true;
             this.dgvAllUsers.BackgroundColor = System.Drawing.Color.LightSalmon;
             this.dgvAllUsers.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvAllUsers.Location = new System.Drawing.Point(30, 271);
+            this.dgvAllUsers.ContextMenuStrip = this.cmsOptionsMenue;
+            this.dgvAllUsers.Location = new System.Drawing.Point(12, 271);
             this.dgvAllUsers.Name = "dgvAllUsers";
             this.dgvAllUsers.ReadOnly = true;
-            this.dgvAllUsers.Size = new System.Drawing.Size(922, 295);
+            this.dgvAllUsers.Size = new System.Drawing.Size(901, 295);
             this.dgvAllUsers.TabIndex = 9;
             // 
             // ManageUsersTitle
@@ -114,7 +116,7 @@
             this.ManageUsersTitle.AutoSize = true;
             this.ManageUsersTitle.Font = new System.Drawing.Font("Palatino Linotype", 24F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.ManageUsersTitle.ForeColor = System.Drawing.Color.Blue;
-            this.ManageUsersTitle.Location = new System.Drawing.Point(365, 168);
+            this.ManageUsersTitle.Location = new System.Drawing.Point(355, 168);
             this.ManageUsersTitle.Name = "ManageUsersTitle";
             this.ManageUsersTitle.Size = new System.Drawing.Size(212, 43);
             this.ManageUsersTitle.TabIndex = 8;
@@ -123,7 +125,7 @@
             // pbManagePeopleForm
             // 
             this.pbManagePeopleForm.Image = global::DVLD_UI.Properties.Resources.management;
-            this.pbManagePeopleForm.Location = new System.Drawing.Point(334, 12);
+            this.pbManagePeopleForm.Location = new System.Drawing.Point(323, 12);
             this.pbManagePeopleForm.Name = "pbManagePeopleForm";
             this.pbManagePeopleForm.Size = new System.Drawing.Size(278, 139);
             this.pbManagePeopleForm.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
@@ -152,6 +154,7 @@
             this.addNewUserToolStripMenuItem.Name = "addNewUserToolStripMenuItem";
             this.addNewUserToolStripMenuItem.Size = new System.Drawing.Size(168, 22);
             this.addNewUserToolStripMenuItem.Text = "Add New User";
+            this.addNewUserToolStripMenuItem.Click += new System.EventHandler(this.addNewUserToolStripMenuItem_Click);
             // 
             // editUserInfoToolStripMenuItem
             // 
@@ -159,6 +162,7 @@
             this.editUserInfoToolStripMenuItem.Name = "editUserInfoToolStripMenuItem";
             this.editUserInfoToolStripMenuItem.Size = new System.Drawing.Size(168, 22);
             this.editUserInfoToolStripMenuItem.Text = "Edit User Info";
+            this.editUserInfoToolStripMenuItem.Click += new System.EventHandler(this.editUserInfoToolStripMenuItem_Click);
             // 
             // deleteUserToolStripMenuItem
             // 
@@ -166,6 +170,7 @@
             this.deleteUserToolStripMenuItem.Name = "deleteUserToolStripMenuItem";
             this.deleteUserToolStripMenuItem.Size = new System.Drawing.Size(168, 22);
             this.deleteUserToolStripMenuItem.Text = "Delete User";
+            this.deleteUserToolStripMenuItem.Click += new System.EventHandler(this.deleteUserToolStripMenuItem_Click);
             // 
             // showUserInfoToolStripMenuItem
             // 
@@ -191,7 +196,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.NavajoWhite;
-            this.ClientSize = new System.Drawing.Size(982, 578);
+            this.ClientSize = new System.Drawing.Size(934, 578);
             this.Controls.Add(this.txtFilter);
             this.Controls.Add(this.SortTitle);
             this.Controls.Add(this.cbSortBy);

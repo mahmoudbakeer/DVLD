726d7d798c762a6f996f6c0af24a3431b36d4ab3
Mahmoud Bakir <mahmoodbaker100@gmail.com>
2026-02-28 23:24:16 +0530

    Add User Info form to display detailed user profile information

---
+++ DVLD_UI/Users/frmShowUserInfo.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace DVLD_UI.Users.Controllers
+{
+    partial class frmShowUserInfo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ctrlUserCard1 = new DVLD_UI.Users.Controllers.ctrlUserCard();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // ctrlUserCard1
+            // 
+            this.ctrlUserCard1.Location = new System.Drawing.Point(12, 78);
+            this.ctrlUserCard1.Name = "ctrlUserCard1";
+            this.ctrlUserCard1.Size = new System.Drawing.Size(854, 402);
+            this.ctrlUserCard1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.label1.Location = new System.Drawing.Point(329, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(223, 31);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "User Information";
+            // 
+            // frmShowUserInfo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(877, 492);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ctrlUserCard1);
+            this.Name = "frmShowUserInfo";
+            this.Text = "frmShowUserInfo";
+            this.Load += new System.EventHandler(this.frmShowUserInfo_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private ctrlUserCard ctrlUserCard1;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ DVLD_UI/Users/frmShowUserInfo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DVLD_UI.Users.Controllers
+{
+    public partial class frmShowUserInfo : Form
+    {
+        private int _UserID;
+        public int UserID
+        {
+            get {  return _UserID; }
+        }
+        public frmShowUserInfo(int UserID)
+        {
+            InitializeComponent();
+            this._UserID = UserID;
+        }
+
+        private void frmShowUserInfo_Load(object sender, EventArgs e)
+        {
+            ctrlUserCard1.LoadUser(UserID);
+        }
+    }
+}

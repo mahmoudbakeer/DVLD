b415b88bc714e166a5880456d900fef957376a29
Mahmoud Bakir <mahmoodbaker100@gmail.com>
2026-02-28 23:24:09 +0530

    Add GlobalUser class to manage logged-in user session across application

---
+++ DVLD_UI/clsGlobalUser.cs
@@ -0,0 +1,14 @@
+﻿using DVLD_BusinessLogic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DVLD_UI
+{
+    public static class clsGlobalUser
+    {
+        public static clsUser gUser = new clsUser();
+    }
+}

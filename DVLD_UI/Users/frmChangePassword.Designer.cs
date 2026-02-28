ef4440db0dabcdea4320c8809cdd15a61765568e
Mahmoud Bakir <mahmoodbaker100@gmail.com>
2026-02-28 23:24:13 +0530

    Add Change Password form with validation and update logic

---
+++ DVLD_UI/Users/frmChangePassword.Designer.cs
@@ -0,0 +1,239 @@
+﻿namespace DVLD_UI.Users
+{
+    partial class frmChangePassword
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.ctrlUserCard1 = new DVLD_UI.Users.Controllers.ctrlUserCard();
+            this.cbIsActive = new System.Windows.Forms.CheckBox();
+            this.lblConfirmPassowrd = new System.Windows.Forms.Label();
+            this.txtConfirmpassword = new System.Windows.Forms.TextBox();
+            this.lblPassowrd = new System.Windows.Forms.Label();
+            this.lblUserName = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.txtUserName = new System.Windows.Forms.TextBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lblMainTitle = new System.Windows.Forms.Label();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ctrlUserCard1
+            // 
+            this.ctrlUserCard1.Location = new System.Drawing.Point(2, 138);
+            this.ctrlUserCard1.Name = "ctrlUserCard1";
+            this.ctrlUserCard1.Size = new System.Drawing.Size(854, 402);
+            this.ctrlUserCard1.TabIndex = 0;
+            // 
+            // cbIsActive
+            // 
+            this.cbIsActive.AutoSize = true;
+            this.cbIsActive.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbIsActive.Location = new System.Drawing.Point(374, 702);
+            this.cbIsActive.Name = "cbIsActive";
+            this.cbIsActive.Size = new System.Drawing.Size(84, 24);
+            this.cbIsActive.TabIndex = 21;
+            this.cbIsActive.Text = "IsActive";
+            this.cbIsActive.UseVisualStyleBackColor = true;
+            // 
+            // lblConfirmPassowrd
+            // 
+            this.lblConfirmPassowrd.AutoSize = true;
+            this.lblConfirmPassowrd.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblConfirmPassowrd.Location = new System.Drawing.Point(177, 653);
+            this.lblConfirmPassowrd.Name = "lblConfirmPassowrd";
+            this.lblConfirmPassowrd.Size = new System.Drawing.Size(143, 22);
+            this.lblConfirmPassowrd.TabIndex = 20;
+            this.lblConfirmPassowrd.Text = "Confirm Password :";
+            // 
+            // txtConfirmpassword
+            // 
+            this.txtConfirmpassword.Location = new System.Drawing.Point(374, 653);
+            this.txtConfirmpassword.Name = "txtConfirmpassword";
+            this.txtConfirmpassword.Size = new System.Drawing.Size(174, 20);
+            this.txtConfirmpassword.TabIndex = 19;
+            // 
+            // lblPassowrd
+            // 
+            this.lblPassowrd.AutoSize = true;
+            this.lblPassowrd.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPassowrd.Location = new System.Drawing.Point(177, 603);
+            this.lblPassowrd.Name = "lblPassowrd";
+            this.lblPassowrd.Size = new System.Drawing.Size(82, 22);
+            this.lblPassowrd.TabIndex = 15;
+            this.lblPassowrd.Text = "Password :";
+            // 
+            // lblUserName
+            // 
+            this.lblUserName.AutoSize = true;
+            this.lblUserName.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUserName.Location = new System.Drawing.Point(177, 555);
+            this.lblUserName.Name = "lblUserName";
+            this.lblUserName.Size = new System.Drawing.Size(92, 22);
+            this.lblUserName.TabIndex = 14;
+            this.lblUserName.Text = "UserName :";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(374, 603);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(174, 20);
+            this.txtPassword.TabIndex = 13;
+            // 
+            // txtUserName
+            // 
+            this.txtUserName.Location = new System.Drawing.Point(374, 558);
+            this.txtUserName.Name = "txtUserName";
+            this.txtUserName.Size = new System.Drawing.Size(174, 20);
+            this.txtUserName.TabIndex = 12;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::DVLD_UI.Properties.Resources.padlock;
+            this.pictureBox3.Location = new System.Drawing.Point(326, 650);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(26, 23);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 18;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::DVLD_UI.Properties.Resources.padlock;
+            this.pictureBox2.Location = new System.Drawing.Point(326, 600);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(26, 23);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 17;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::DVLD_UI.Properties.Resources.user;
+            this.pictureBox1.Location = new System.Drawing.Point(326, 555);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(26, 23);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 16;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lblMainTitle
+            // 
+            this.lblMainTitle.AutoSize = true;
+            this.lblMainTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMainTitle.ForeColor = System.Drawing.Color.Blue;
+            this.lblMainTitle.Location = new System.Drawing.Point(301, 60);
+            this.lblMainTitle.Name = "lblMainTitle";
+            this.lblMainTitle.Size = new System.Drawing.Size(225, 29);
+            this.lblMainTitle.TabIndex = 22;
+            this.lblMainTitle.Text = "Change Passowrd";
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // btnSave
+            // 
+            this.btnSave.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnSave.Location = new System.Drawing.Point(654, 577);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(109, 33);
+            this.btnSave.TabIndex = 23;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = false;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnCancel.Location = new System.Drawing.Point(654, 642);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(109, 33);
+            this.btnCancel.TabIndex = 24;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = false;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // frmChangePassword
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.ClientSize = new System.Drawing.Size(856, 757);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.lblMainTitle);
+            this.Controls.Add(this.cbIsActive);
+            this.Controls.Add(this.lblConfirmPassowrd);
+            this.Controls.Add(this.txtConfirmpassword);
+            this.Controls.Add(this.lblPassowrd);
+            this.Controls.Add(this.lblUserName);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtUserName);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.ctrlUserCard1);
+            this.Name = "frmChangePassword";
+            this.Text = "frmChangePassword";
+            this.Load += new System.EventHandler(this.frmChangePassword_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Controllers.ctrlUserCard ctrlUserCard1;
+        private System.Windows.Forms.CheckBox cbIsActive;
+        private System.Windows.Forms.Label lblConfirmPassowrd;
+        private System.Windows.Forms.TextBox txtConfirmpassword;
+        private System.Windows.Forms.Label lblPassowrd;
+        private System.Windows.Forms.Label lblUserName;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.TextBox txtUserName;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lblMainTitle;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnSave;
+    }
+}
---
+++ DVLD_UI/Users/frmChangePassword.cs
@@ -0,0 +1,111 @@
+﻿using DVLD_BusinessLogic;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DVLD_UI.Users
+{
+    public partial class frmChangePassword : Form
+    {
+        private int _UserID;
+        private clsUser _User;
+        public clsUser User
+        {
+            get { return _User; }
+        }
+        public int UserID
+        {
+            get { return _UserID; }
+        }
+        public frmChangePassword(int UserID)
+        {
+            InitializeComponent();
+            this._UserID = UserID;
+        }
+        public void LoadInfo(int UserID)
+        {
+            
+            _User = clsUser.Find(UserID);
+            if (User == null) return;
+            else
+            {
+                ctrlUserCard1.LoadUser(UserID);
+                txtUserName.Text = User.UserName;
+                txtPassword.Text = User.Password;
+                txtConfirmpassword.Text = User.Password;
+            }
+        }
+        private void _FillInfo()
+        {
+            if (!_validatetxtboxes()) return;
+            _User.UserName = txtUserName.Text.Trim();
+            _User.Password = txtPassword.Text.Trim();
+            _User.IsActive = cbIsActive.Checked;
+        }
+        private bool _validatetxtboxes()
+        {
+            if (string.IsNullOrEmpty(txtUserName.Text))
+            {
+                errorProvider1.SetError(txtUserName, "Cannot be empty!");
+                txtUserName.Focus();
+                return false;
+            }
+            else if (clsUser.IsUserNameExistForAnotherUser(txtUserName.Text, UserID))
+            {
+                errorProvider1.SetError(txtUserName, "UserName is taken, choose another!");
+                txtUserName.Clear();
+                txtUserName.Focus();
+                return false;
+            }
+            if (string.IsNullOrEmpty(txtPassword.Text))
+            {
+                errorProvider1.SetError(txtPassword, "Cannot be empty!");
+                txtPassword.Focus();
+                return false;
+            }
+            if (string.IsNullOrEmpty(txtConfirmpassword.Text))
+            {
+                errorProvider1.SetError(txtConfirmpassword, "Cannot be empty!");
+                txtConfirmpassword.Focus();
+                return false;
+            }
+            else if (txtConfirmpassword.Text.Trim() != txtPassword.Text.Trim())
+            {
+                errorProvider1.SetError(txtConfirmpassword, "Confirmation Is wrong!!");
+                txtConfirmpassword.Focus();
+                return false;
+            }
+            return true;
+        }
+        private void frmChangePassword_Load(object sender, EventArgs e)
+        {
+            LoadInfo(UserID);
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if (!_validatetxtboxes()) return;
+            _FillInfo();
+            if(User.Save())
+            {
+                MessageBox.Show("Done,Informations Updated Successfully!","Successeded",MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("Failed,Couldnt Update the information!","Failed",MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}

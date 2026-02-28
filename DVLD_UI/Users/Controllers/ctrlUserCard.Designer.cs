604ebf313fdead9be1fa396d416efce6c5168b04
Mahmoud Bakir <mahmoodbaker100@gmail.com>
2026-02-28 23:24:01 +0530

    Introduce Users controllers to manage form interactions and logic flow

---
+++ DVLD_UI/Users/Controllers/ctrlUserCard.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace DVLD_UI.Users.Controllers
+{
+    partial class ctrlUserCard
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ctrlPersonCard1 = new DVLD_UI.People.controls.ctrlPersonCard();
+            this.gbUserInfo = new System.Windows.Forms.GroupBox();
+            this.lblUserNameTitle = new System.Windows.Forms.Label();
+            this.lblUserIdTitle = new System.Windows.Forms.Label();
+            this.lblIsActiveTitle = new System.Windows.Forms.Label();
+            this.lblUserName = new System.Windows.Forms.Label();
+            this.lblUserID = new System.Windows.Forms.Label();
+            this.lblIsActive = new System.Windows.Forms.Label();
+            this.gbUserInfo.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // ctrlPersonCard1
+            // 
+            this.ctrlPersonCard1.BackColor = System.Drawing.Color.LightSalmon;
+            this.ctrlPersonCard1.Location = new System.Drawing.Point(0, 0);
+            this.ctrlPersonCard1.Name = "ctrlPersonCard1";
+            this.ctrlPersonCard1.Size = new System.Drawing.Size(853, 296);
+            this.ctrlPersonCard1.TabIndex = 0;
+            // 
+            // gbUserInfo
+            // 
+            this.gbUserInfo.Controls.Add(this.lblIsActive);
+            this.gbUserInfo.Controls.Add(this.lblUserID);
+            this.gbUserInfo.Controls.Add(this.lblUserName);
+            this.gbUserInfo.Controls.Add(this.lblIsActiveTitle);
+            this.gbUserInfo.Controls.Add(this.lblUserIdTitle);
+            this.gbUserInfo.Controls.Add(this.lblUserNameTitle);
+            this.gbUserInfo.Location = new System.Drawing.Point(3, 299);
+            this.gbUserInfo.Name = "gbUserInfo";
+            this.gbUserInfo.Size = new System.Drawing.Size(848, 100);
+            this.gbUserInfo.TabIndex = 1;
+            this.gbUserInfo.TabStop = false;
+            this.gbUserInfo.Text = "UsertInfo";
+            // 
+            // lblUserNameTitle
+            // 
+            this.lblUserNameTitle.AutoSize = true;
+            this.lblUserNameTitle.Font = new System.Drawing.Font("Palatino Linotype", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUserNameTitle.Location = new System.Drawing.Point(6, 33);
+            this.lblUserNameTitle.Name = "lblUserNameTitle";
+            this.lblUserNameTitle.Size = new System.Drawing.Size(125, 28);
+            this.lblUserNameTitle.TabIndex = 0;
+            this.lblUserNameTitle.Text = "UserName :";
+            // 
+            // lblUserIdTitle
+            // 
+            this.lblUserIdTitle.AutoSize = true;
+            this.lblUserIdTitle.Font = new System.Drawing.Font("Palatino Linotype", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUserIdTitle.Location = new System.Drawing.Point(303, 33);
+            this.lblUserIdTitle.Name = "lblUserIdTitle";
+            this.lblUserIdTitle.Size = new System.Drawing.Size(95, 28);
+            this.lblUserIdTitle.TabIndex = 1;
+            this.lblUserIdTitle.Text = "User ID :";
+            // 
+            // lblIsActiveTitle
+            // 
+            this.lblIsActiveTitle.AutoSize = true;
+            this.lblIsActiveTitle.Font = new System.Drawing.Font("Palatino Linotype", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblIsActiveTitle.Location = new System.Drawing.Point(574, 33);
+            this.lblIsActiveTitle.Name = "lblIsActiveTitle";
+            this.lblIsActiveTitle.Size = new System.Drawing.Size(105, 28);
+            this.lblIsActiveTitle.TabIndex = 2;
+            this.lblIsActiveTitle.Text = "Is Active :";
+            // 
+            // lblUserName
+            // 
+            this.lblUserName.AutoSize = true;
+            this.lblUserName.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUserName.ForeColor = System.Drawing.Color.Red;
+            this.lblUserName.Location = new System.Drawing.Point(152, 37);
+            this.lblUserName.Name = "lblUserName";
+            this.lblUserName.Size = new System.Drawing.Size(50, 23);
+            this.lblUserName.TabIndex = 3;
+            this.lblUserName.Text = "#####";
+            // 
+            // lblUserID
+            // 
+            this.lblUserID.AutoSize = true;
+            this.lblUserID.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUserID.ForeColor = System.Drawing.Color.Red;
+            this.lblUserID.Location = new System.Drawing.Point(433, 37);
+            this.lblUserID.Name = "lblUserID";
+            this.lblUserID.Size = new System.Drawing.Size(50, 23);
+            this.lblUserID.TabIndex = 4;
+            this.lblUserID.Text = "#####";
+            // 
+            // lblIsActive
+            // 
+            this.lblIsActive.AutoSize = true;
+            this.lblIsActive.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblIsActive.ForeColor = System.Drawing.Color.Red;
+            this.lblIsActive.Location = new System.Drawing.Point(710, 37);
+            this.lblIsActive.Name = "lblIsActive";
+            this.lblIsActive.Size = new System.Drawing.Size(50, 23);
+            this.lblIsActive.TabIndex = 5;
+            this.lblIsActive.Text = "#####";
+            // 
+            // ctrlUserCard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.gbUserInfo);
+            this.Controls.Add(this.ctrlPersonCard1);
+            this.Name = "ctrlUserCard";
+            this.Size = new System.Drawing.Size(854, 402);
+            this.gbUserInfo.ResumeLayout(false);
+            this.gbUserInfo.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private People.controls.ctrlPersonCard ctrlPersonCard1;
+        private System.Windows.Forms.GroupBox gbUserInfo;
+        private System.Windows.Forms.Label lblUserNameTitle;
+        private System.Windows.Forms.Label lblIsActiveTitle;
+        private System.Windows.Forms.Label lblUserIdTitle;
+        private System.Windows.Forms.Label lblIsActive;
+        private System.Windows.Forms.Label lblUserID;
+        private System.Windows.Forms.Label lblUserName;
+    }
+}
---
+++ DVLD_UI/Users/Controllers/ctrlUserCard.cs
@@ -0,0 +1,52 @@
+﻿using DVLD_BusinessLogic;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DVLD_UI.Users.Controllers
+{
+    public partial class ctrlUserCard : UserControl
+    {
+        private clsUser _User;
+        public clsUser User
+            { get { return _User; } }
+        private int _UserID;
+        public int UserID
+        {
+            get {
+                return _UserID;
+            }
+        }
+        public ctrlUserCard()
+        {
+            InitializeComponent();
+        }
+        private void _SetInfo()
+        {
+            ctrlPersonCard1.LoadPersonInfo(User.PersonID);
+            lblIsActive.Text = User.IsActive ? "YES": "NO";
+            lblUserID.Text = User.ID.ToString();
+            lblUserName.Text = User.UserName.ToString();
+
+        }
+
+        public void LoadUser(int UserID)
+        {
+            _User = clsUser.Find(UserID);
+            if (_User == null) return;
+            else
+            {
+                _UserID = UserID;
+                _SetInfo();
+                return;
+            }
+        }
+    }
+}

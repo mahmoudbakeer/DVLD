5bb2a54d039934fe262d393abea07f01539e3b9d
Mahmoud Bakir <mahmoodbaker100@gmail.com>
2026-03-01 14:59:56 +0530

    Regenerate resource designer after adding new UI assets

--- DVLD_UI/Properties/Resources.Designer.cs
+++ DVLD_UI/Properties/Resources.Designer.cs
@@ -203,6 +203,16 @@ namespace DVLD_UI.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap logout_4400828 {
+            get {
+                object obj = ResourceManager.GetObject("logout_4400828", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap mail {
             get {
                 object obj = ResourceManager.GetObject("mail", resourceCulture);

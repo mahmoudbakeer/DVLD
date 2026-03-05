4dd138b826f9d07817e4244a435e56d18a6d915d
Mahmoud Bakir <mahmoodbaker100@gmail.com>
2026-03-05 19:29:24 +0530

    Add business logic class for Test Type to manage test type rules

---
+++ DVLD_BusinessLogic/clsTestType.cs
@@ -0,0 +1,57 @@
+﻿using DVLD_DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DVLD_BusinessLogic
+{
+    public class clsTestType
+    {
+        public int TestTypeID
+        {
+            get; set;
+        }
+        public string TestTypeTitle { get; set; }
+        public string TestTypeDescribtion { get; set; }
+        public decimal TestTypeFees { get; set; }
+
+        public clsTestType(int TestTypeID, string TestTypeTitle,string TestTypeDescribtion, decimal TestTypeFees)
+        {
+            this.TestTypeID = TestTypeID;
+            this.TestTypeTitle = TestTypeTitle;
+            this.TestTypeDescribtion= TestTypeDescribtion;
+            this.TestTypeFees = TestTypeFees;
+        }
+        public clsTestType()
+        {
+            this.TestTypeID = -1;
+            this.TestTypeTitle = string.Empty;
+            this.TestTypeDescribtion = string.Empty;
+            this.TestTypeFees = 0;
+        }
+
+
+
+        public static clsTestType GetTestType(int TestTypeID)
+        {
+            string TestTypeTitle = string.Empty;
+            string TestTypeDescribtion = string.Empty;
+            decimal TestTypeFees = 0;
+
+            if (clsTestTypesDataAccess.GetTestTypeByID(TestTypeID, ref TestTypeTitle,ref TestTypeDescribtion, ref TestTypeFees))
+                return new clsTestType(TestTypeID, TestTypeTitle,TestTypeDescribtion, TestTypeFees);
+            else return null;
+        }
+        public bool UpdateTestType()
+        {
+            return clsTestTypesDataAccess.UpdateTestType(TestTypeID,TestTypeTitle,TestTypeDescribtion,TestTypeFees);
+        }
+        public static DataTable GetAllTestTypes()
+        {
+            return clsTestTypesDataAccess.GetAllTestTypes();
+        }
+    }
+}

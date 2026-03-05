66043a04b6bde178a4a7e0dded952fcd31d1871a
Mahmoud Bakir <mahmoodbaker100@gmail.com>
2026-03-05 19:28:57 +0530

    Add data access class for managing Test Types operations

---
+++ DVLD_DataAccess/clsTestTypesDataAccess.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DVLD_DataAccess
+{
+    public class clsTestTypesDataAccess
+    {
+        public static bool GetTestTypeByID(int ID, ref string TestTypeTitle,ref string TestTypeDescription, ref decimal TestTypeFees)
+        {
+            bool isFound = false;
+
+            SqlConnection connection = new SqlConnection(clsDataAccessSettings.ConnectionString);
+
+            string query = "SELECT * FROM TestTypes WHERE TestTypeID = @TestTypeID";
+
+            SqlCommand command = new SqlCommand(query, connection);
+
+            command.Parameters.AddWithValue("@TestTypeID", ID);
+
+            try
+            {
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+
+                if (reader.Read())
+                {
+
+                    // The record was found
+                    isFound = true;
+                    TestTypeDescription = (string)reader["TestTypeDescription"];
+                    TestTypeTitle = (string)reader["TestTypeTitle"];
+                    TestTypeFees = (decimal)reader["TestTypeFees"];
+
+                }
+                else
+                {
+                    // The record was not found
+                    isFound = false;
+                }
+
+                reader.Close();
+
+
+            }
+            catch (Exception ex)
+            {
+                //Console.WriteLine("Error: " + ex.Message);
+                isFound = false;
+            }
+            finally
+            {
+                connection.Close();
+            }
+
+            return isFound;
+        }
+
+
+        public static bool UpdateTestType(int ID, string TestTypeTitle,string TestTypeDescription, decimal TestTypeFees)
+        {
+
+            int rowsAffected = 0;
+            SqlConnection connection = new SqlConnection(clsDataAccessSettings.ConnectionString);
+
+            string query = @"Update  TestTypes  
+                            set TestTypeTitle =@TestTypeTitle , 
+                            TestTypeDescription = @TestTypeDescription, 
+                             TestTypeFees =  @TestTypeFees
+                                where TestTypeID = @TestTypeID";
+
+            SqlCommand command = new SqlCommand(query, connection);
+
+            command.Parameters.AddWithValue("@TestTypeID", ID);
+            command.Parameters.AddWithValue("@TestTypeFees", TestTypeFees);
+            command.Parameters.AddWithValue("@TestTypeDescription", TestTypeDescription);
+            command.Parameters.AddWithValue("@TestTypeTitle", TestTypeTitle);
+
+            try
+            {
+                connection.Open();
+                rowsAffected = command.ExecuteNonQuery();
+
+            }
+            catch (Exception ex)
+            {
+                //Console.WriteLine("Error: " + ex.Message);
+                return false;
+            }
+
+            finally
+            {
+                connection.Close();
+            }
+
+            return (rowsAffected > 0);
+        }
+
+        public static DataTable GetAllTestTypes()
+        {
+
+            DataTable dt = new DataTable();
+            SqlConnection connection = new SqlConnection(clsDataAccessSettings.ConnectionString);
+
+            string query = "SELECT TestTypeID , TestTypeTitle,TestTypeDescription , TestTypeFees FROM TestTypes order by TestTypeID";
+
+            SqlCommand command = new SqlCommand(query, connection);
+
+            try
+            {
+                connection.Open();
+
+                SqlDataReader reader = command.ExecuteReader();
+
+                if (reader.HasRows)
+
+                {
+                    dt.Load(reader);
+                }
+
+                reader.Close();
+
+
+            }
+
+            catch (Exception ex)
+            {
+                // Console.WriteLine("Error: " + ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+
+            return dt;
+
+        }
+    }
+}

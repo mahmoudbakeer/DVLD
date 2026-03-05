8e7c74de5e32e030e828785c1dff7d3dc82df7b1
Mahmoud Bakir <mahmoodbaker100@gmail.com>
2026-03-05 19:29:39 +0530

    Update design of Update Application Type form

--- DVLD_UI/Applications/frmUpdateApplicationType.Designer.cs
+++ DVLD_UI/Applications/frmUpdateApplicationType.Designer.cs
@@ -28,7 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.lblMainTitle = new System.Windows.Forms.Label();
+            this.txtApplicationTitle = new System.Windows.Forms.TextBox();
+            this.txtApplicationFees = new System.Windows.Forms.TextBox();
+            this.lblApplicationIDTitle = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblAppID = new System.Windows.Forms.Label();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
             this.SuspendLayout();
             // 
             // lblMainTitle
@@ -36,20 +47,115 @@
             this.lblMainTitle.AutoSize = true;
             this.lblMainTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblMainTitle.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-            this.lblMainTitle.Location = new System.Drawing.Point(116, 47);
+            this.lblMainTitle.Location = new System.Drawing.Point(193, 36);
             this.lblMainTitle.Name = "lblMainTitle";
             this.lblMainTitle.Size = new System.Drawing.Size(197, 29);
             this.lblMainTitle.TabIndex = 0;
             this.lblMainTitle.Text = "Update Type Info";
             // 
+            // txtApplicationTitle
+            // 
+            this.txtApplicationTitle.Location = new System.Drawing.Point(286, 241);
+            this.txtApplicationTitle.Name = "txtApplicationTitle";
+            this.txtApplicationTitle.Size = new System.Drawing.Size(259, 20);
+            this.txtApplicationTitle.TabIndex = 1;
+            // 
+            // txtApplicationFees
+            // 
+            this.txtApplicationFees.Location = new System.Drawing.Point(286, 299);
+            this.txtApplicationFees.Name = "txtApplicationFees";
+            this.txtApplicationFees.Size = new System.Drawing.Size(259, 20);
+            this.txtApplicationFees.TabIndex = 2;
+            this.txtApplicationFees.Validating += new System.ComponentModel.CancelEventHandler(this.txtApplicationFees_Validating);
+            // 
+            // lblApplicationIDTitle
+            // 
+            this.lblApplicationIDTitle.AutoSize = true;
+            this.lblApplicationIDTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblApplicationIDTitle.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.lblApplicationIDTitle.Location = new System.Drawing.Point(221, 168);
+            this.lblApplicationIDTitle.Name = "lblApplicationIDTitle";
+            this.lblApplicationIDTitle.Size = new System.Drawing.Size(54, 29);
+            this.lblApplicationIDTitle.TabIndex = 3;
+            this.lblApplicationIDTitle.Text = "ID : ";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.label1.Location = new System.Drawing.Point(15, 232);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(265, 29);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Application Type Title : ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.label2.Location = new System.Drawing.Point(15, 299);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(272, 29);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Application Type Fees : ";
+            // 
+            // lblAppID
+            // 
+            this.lblAppID.AutoSize = true;
+            this.lblAppID.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAppID.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.lblAppID.Location = new System.Drawing.Point(281, 168);
+            this.lblAppID.Name = "lblAppID";
+            this.lblAppID.Size = new System.Drawing.Size(52, 29);
+            this.lblAppID.TabIndex = 6;
+            this.lblAppID.Text = "###";
+            // 
+            // btnSave
+            // 
+            this.btnSave.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnSave.Location = new System.Drawing.Point(321, 345);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(90, 29);
+            this.btnSave.TabIndex = 7;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = false;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnCancel.Location = new System.Drawing.Point(185, 345);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(90, 29);
+            this.btnCancel.TabIndex = 8;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = false;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
             // frmUpdateApplicationType
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(453, 617);
+            this.ClientSize = new System.Drawing.Size(583, 386);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.lblAppID);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblApplicationIDTitle);
+            this.Controls.Add(this.txtApplicationFees);
+            this.Controls.Add(this.txtApplicationTitle);
             this.Controls.Add(this.lblMainTitle);
             this.Name = "frmUpdateApplicationType";
             this.Text = "frmUpdateApplicationType";
+            this.Load += new System.EventHandler(this.frmUpdateApplicationType_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -58,5 +164,14 @@
         #endregion
 
         private System.Windows.Forms.Label lblMainTitle;
+        private System.Windows.Forms.TextBox txtApplicationTitle;
+        private System.Windows.Forms.TextBox txtApplicationFees;
+        private System.Windows.Forms.Label lblApplicationIDTitle;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblAppID;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
     }
 }

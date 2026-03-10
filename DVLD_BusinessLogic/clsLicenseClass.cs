863ad8560458e72e0985d704c1f0838b7bc25325
Mahmoud Bakir <mahmoodbaker100@gmail.com>
2026-03-10 19:59:42 +0530

    Refactor LicenseClass business logic and improve data handling

--- DVLD_BusinessLogic/clsLicenseClass.cs
+++ DVLD_BusinessLogic/clsLicenseClass.cs
@@ -8,20 +8,20 @@ using System.Threading.Tasks;
 
 namespace DVLD_BusinessLogic
 {
-    internal class clsLicenseClass
+    public class clsLicenseClass
     {
-            int LicenseClassID
+            public int LicenseClassID
         {
             get; set;
         }
-            string ClassName
+            public string ClassName
         {
             get; set;
         }
-            string ClassDescription { get; set; }
-            int MinimumAllowedAge {  get; set; }
-            int DefaultValiditLength {  get; set; }
-            decimal ClassFees {  get; set; }
+            public string ClassDescription { get; set; }
+            public int MinimumAllowedAge {  get; set; }
+            public int DefaultValiditLength {  get; set; }
+            public decimal ClassFees {  get; set; }
         clsLicenseClass(int LicenseClassID,string ClassName,string ClassDescription,int MinimumAllowedAge , int DefaultValiditLength , decimal ClassFees)
         {
             this.ClassName = ClassName;
@@ -44,6 +44,28 @@ namespace DVLD_BusinessLogic
         //{
         //    return (clsLicenseClassesDataAccess.UpdateLicenseClass(this.LicenseClassID, this.ClassName, this.ClassDescription, this.MinimumAllowedAge,this.DefaultValiditLength , this.ClassFees));
         //}
+        public static clsLicenseClass GetLicenseClass(int ClassID)
+        {
+            int MinimumAllowedAge = -1,DefaultValidtyLength = -1;
+            decimal ClassFees = 0;
+            string ClassDescription = string.Empty,ClassName = string.Empty ;
+            if (clsLicenseClassesDataAccess.GetLicenseClassByID(ClassID,ref ClassName,ref ClassDescription,ref MinimumAllowedAge,ref DefaultValidtyLength,ref ClassFees))
+            {
+                return new clsLicenseClass(ClassID,ClassName,ClassDescription,MinimumAllowedAge,DefaultValidtyLength,ClassFees);
+            }
+            else return null;
+        }
+        public static clsLicenseClass GetLicenseClass(string ClassName)
+        {
+            int MinimumAllowedAge = -1, DefaultValidtyLength = -1, ClassID = -1;
+            decimal ClassFees = 0;
+            string ClassDescription = string.Empty;
+            if (clsLicenseClassesDataAccess.GetLicenseClassByName(ref ClassID,  ClassName, ref ClassDescription, ref MinimumAllowedAge, ref DefaultValidtyLength, ref ClassFees))
+            {
+                return new clsLicenseClass(ClassID, ClassName, ClassDescription, MinimumAllowedAge, DefaultValidtyLength, ClassFees);
+            }
+            else return null;
+        }
         public static DataTable GetAllLicenses()
         {
             DataTable dataTable = clsLicenseClassesDataAccess.GetAllLicenseClasses();

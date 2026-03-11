6183fa48efd4d7b817432fc3f26a2c323f003705
Mahmoud Bakir <mahmoodbaker100@gmail.com>
2026-03-11 18:20:26 +0530

    Add form to display Local Driving License Application details

---
+++ DVLD_UI/Applications/LocalDrivingApplications/frmShowLocalDrivingLicenseApplicationDetails.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace DVLD_UI.Applications.LocalDrivingApplications
+{
+    partial class frmShowLocalDrivingLicenseApplicationDetails
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ctrlLocalDrivingLicenseApplicationInfo1 = new DVLD_UI.Applications.LocalDrivingApplications.Controllers.ctrlLocalDrivingLicenseApplicationInfo();
+            this.lblMainTitle = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // ctrlLocalDrivingLicenseApplicationInfo1
+            // 
+            this.ctrlLocalDrivingLicenseApplicationInfo1.Location = new System.Drawing.Point(2, 87);
+            this.ctrlLocalDrivingLicenseApplicationInfo1.Name = "ctrlLocalDrivingLicenseApplicationInfo1";
+            this.ctrlLocalDrivingLicenseApplicationInfo1.Size = new System.Drawing.Size(829, 430);
+            this.ctrlLocalDrivingLicenseApplicationInfo1.TabIndex = 0;
+            // 
+            // lblMainTitle
+            // 
+            this.lblMainTitle.AutoSize = true;
+            this.lblMainTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMainTitle.ForeColor = System.Drawing.Color.Red;
+            this.lblMainTitle.Location = new System.Drawing.Point(241, 37);
+            this.lblMainTitle.Name = "lblMainTitle";
+            this.lblMainTitle.Size = new System.Drawing.Size(309, 24);
+            this.lblMainTitle.TabIndex = 1;
+            this.lblMainTitle.Text = "Local Driving Application Details";
+            // 
+            // frmShowLocalDrivingLicenseApplicationDetails
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(824, 516);
+            this.Controls.Add(this.lblMainTitle);
+            this.Controls.Add(this.ctrlLocalDrivingLicenseApplicationInfo1);
+            this.Name = "frmShowLocalDrivingLicenseApplicationDetails";
+            this.Text = "frmShowLocalDrivingLicenseApplicationDetails";
+            this.Load += new System.EventHandler(this.frmShowLocalDrivingLicenseApplicationDetails_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Controllers.ctrlLocalDrivingLicenseApplicationInfo ctrlLocalDrivingLicenseApplicationInfo1;
+        private System.Windows.Forms.Label lblMainTitle;
+    }
+}
---
+++ DVLD_UI/Applications/LocalDrivingApplications/frmShowLocalDrivingLicenseApplicationDetails.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DVLD_UI.Applications.LocalDrivingApplications
+{
+    public partial class frmShowLocalDrivingLicenseApplicationDetails : Form
+    {
+        private int _LDLA;
+        public int LDLA
+        {
+            get {  return _LDLA; }
+        }
+        public frmShowLocalDrivingLicenseApplicationDetails(int LocalDrivingLicenseApplicationID)
+        {
+            InitializeComponent();
+            _LDLA = LocalDrivingLicenseApplicationID;
+        }
+
+        private void frmShowLocalDrivingLicenseApplicationDetails_Load(object sender, EventArgs e)
+        {
+            ctrlLocalDrivingLicenseApplicationInfo1.LoadDLAInformation(_LDLA);
+        }
+    }
+}

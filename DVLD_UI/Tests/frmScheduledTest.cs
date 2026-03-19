51788312e4645cdf1f64c0aa2f5ac7116a1e7d58
Mahmoud Bakir <mahmoodbaker100@gmail.com>
2026-03-19 23:46:59 +0530

    Enhance scheduled test management form
    - Improved display of scheduled tests.
    - Added better handling for test results and updates.

--- DVLD_UI/Tests/frmScheduledTest.cs
+++ DVLD_UI/Tests/frmScheduledTest.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static DVLD_BusinessLogic.clsTestType;
 
 namespace DVLD_UI.Tests
 {
@@ -31,6 +32,19 @@ namespace DVLD_UI.Tests
         private void frmScheduledTest_Load(object sender, EventArgs e)
         {
             ctrlTestTypeInfo1.LoadTestTypeInfo(_LocalDrivingLicenseApplicationID,_TestType);
+            _Appointment = clsTestAppointment.Find(AppointmentID);
+            if( _Appointment == null )
+            {
+                MessageBox.Show("This Appointment is not valid please make sure the id is correct", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                this.Enabled = false;
+                return;
+            }
+            if (clsLocalDrivingLicenseApplication.DoestPersonPassTheTest(_LocalDrivingLicenseApplicationID, (int)_TestType))
+            {
+                MessageBox.Show("This Person Have Already passed this test you cannot add new appointment", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                this.Enabled = false;
+                return;
+            }
         }
 
         private void btnCancel_Click(object sender, EventArgs e)
@@ -49,7 +63,22 @@ namespace DVLD_UI.Tests
         private void btnSave_Click(object sender, EventArgs e)
         {
             if (!_ValidateResult()) return;
-            
+            clsTest Test = new clsTest();
+            Test.TestAppointmentID = _AppointmentID;
+            Test.TestResult = (rbtnPass.Checked && !rbtnFail.Checked) ? true : false;
+            Test.Notes = textBox1.Text;
+            Test.CreatedByUserID = clsGlobalUser.gUser.ID;
+            _Appointment.IsLocked = true;
+            if(Test.Save() && _Appointment.Save())
+            {
+                MessageBox.Show("Succeseded", "Test Result Subitted Successfully", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+            else
+            {
+                MessageBox.Show("Error", "Test Result Was Not Subitted ", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
         }
     }
 }

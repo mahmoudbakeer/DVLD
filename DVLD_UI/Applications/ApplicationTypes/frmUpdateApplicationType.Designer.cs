af2d5c20b6751d2b59e57324a5738d96c7635d4a
Mahmoud Bakir <mahmoodbaker100@gmail.com>
2026-03-25 15:33:09 +0530

    Update Application Type editing form UI for better user interaction

--- DVLD_UI/Applications/ApplicationTypes/frmUpdateApplicationType.Designer.cs
+++ DVLD_UI/Applications/ApplicationTypes/frmUpdateApplicationType.Designer.cs
@@ -142,6 +142,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Chocolate;
             this.ClientSize = new System.Drawing.Size(583, 386);
             this.Controls.Add(this.btnCancel);
             this.Controls.Add(this.btnSave);

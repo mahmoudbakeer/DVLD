55f0edd68548deeb6259cce580a381b03c90bc87
Mahmoud Bakir <mahmoodbaker100@gmail.com>
2026-03-25 15:29:28 +0530

    Add test appointments module for scheduling and managing tests

---
+++ DVLD_UI/Tests/TestAppointments/frmManageTestAppointments.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace DVLD_UI.Tests.TestAppointments
+{
+    partial class frmManageTestAppointments
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblMainTitle = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dgvTestAppoiments = new System.Windows.Forms.DataGridView();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.editTestAppointmentToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.takeTestToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnAddAppointment = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.ctrlLocalDrivingLicenseApplicationInfo1 = new DVLD_UI.Applications.LocalDrivingApplications.Controllers.ctrlLocalDrivingLicenseApplicationInfo();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTestAppoiments)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblMainTitle
+            // 
+            this.lblMainTitle.AutoSize = true;
+            this.lblMainTitle.BackColor = System.Drawing.Color.Bisque;
+            this.lblMainTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMainTitle.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.lblMainTitle.Location = new System.Drawing.Point(355, 58);
+            this.lblMainTitle.Name = "lblMainTitle";
+            this.lblMainTitle.Size = new System.Drawing.Size(202, 29);
+            this.lblMainTitle.TabIndex = 2;
+            this.lblMainTitle.Text = "Test Appointment";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(45, 584);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(151, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Test Appointments :";
+            // 
+            // dgvTestAppoiments
+            // 
+            this.dgvTestAppoiments.AllowUserToAddRows = false;
+            this.dgvTestAppoiments.AllowUserToDeleteRows = false;
+            this.dgvTestAppoiments.AllowUserToOrderColumns = true;
+            this.dgvTestAppoiments.BackgroundColor = System.Drawing.Color.White;
+            this.dgvTestAppoiments.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvTestAppoiments.ContextMenuStrip = this.contextMenuStrip1;
+            this.dgvTestAppoiments.Location = new System.Drawing.Point(49, 626);
+            this.dgvTestAppoiments.Name = "dgvTestAppoiments";
+            this.dgvTestAppoiments.ReadOnly = true;
+            this.dgvTestAppoiments.Size = new System.Drawing.Size(829, 188);
+            this.dgvTestAppoiments.TabIndex = 4;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.editTestAppointmentToolStripMenuItem,
+            this.takeTestToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(193, 48);
+            // 
+            // editTestAppointmentToolStripMenuItem
+            // 
+            this.editTestAppointmentToolStripMenuItem.Image = global::DVLD_UI.Properties.Resources.update;
+            this.editTestAppointmentToolStripMenuItem.Name = "editTestAppointmentToolStripMenuItem";
+            this.editTestAppointmentToolStripMenuItem.Size = new System.Drawing.Size(192, 22);
+            this.editTestAppointmentToolStripMenuItem.Text = "Edit Test Appointment";
+            this.editTestAppointmentToolStripMenuItem.Click += new System.EventHandler(this.editTestAppointmentToolStripMenuItem_Click);
+            // 
+            // takeTestToolStripMenuItem
+            // 
+            this.takeTestToolStripMenuItem.Image = global::DVLD_UI.Properties.Resources.WrittenTest;
+            this.takeTestToolStripMenuItem.Name = "takeTestToolStripMenuItem";
+            this.takeTestToolStripMenuItem.Size = new System.Drawing.Size(192, 22);
+            this.takeTestToolStripMenuItem.Text = "Take Test";
+            this.takeTestToolStripMenuItem.Click += new System.EventHandler(this.takeTestToolStripMenuItem_Click);
+            // 
+            // btnAddAppointment
+            // 
+            this.btnAddAppointment.BackColor = System.Drawing.Color.MistyRose;
+            this.btnAddAppointment.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAddAppointment.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.btnAddAppointment.Location = new System.Drawing.Point(760, 581);
+            this.btnAddAppointment.Name = "btnAddAppointment";
+            this.btnAddAppointment.Size = new System.Drawing.Size(118, 39);
+            this.btnAddAppointment.TabIndex = 5;
+            this.btnAddAppointment.Text = "Add New Appointment";
+            this.btnAddAppointment.UseVisualStyleBackColor = false;
+            this.btnAddAppointment.Click += new System.EventHandler(this.btnAddAppointment_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnCancel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancel.Location = new System.Drawing.Point(778, 820);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(100, 35);
+            this.btnCancel.TabIndex = 6;
+            this.btnCancel.Text = "Close";
+            this.btnCancel.UseVisualStyleBackColor = false;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // ctrlLocalDrivingLicenseApplicationInfo1
+            // 
+            this.ctrlLocalDrivingLicenseApplicationInfo1.BackColor = System.Drawing.Color.White;
+            this.ctrlLocalDrivingLicenseApplicationInfo1.Location = new System.Drawing.Point(49, 127);
+            this.ctrlLocalDrivingLicenseApplicationInfo1.Name = "ctrlLocalDrivingLicenseApplicationInfo1";
+            this.ctrlLocalDrivingLicenseApplicationInfo1.Size = new System.Drawing.Size(829, 430);
+            this.ctrlLocalDrivingLicenseApplicationInfo1.TabIndex = 1;
+            // 
+            // frmManageTestAppointments
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Salmon;
+            this.ClientSize = new System.Drawing.Size(942, 858);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnAddAppointment);
+            this.Controls.Add(this.dgvTestAppoiments);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblMainTitle);
+            this.Controls.Add(this.ctrlLocalDrivingLicenseApplicationInfo1);
+            this.Name = "frmManageTestAppointments";
+            this.Text = "frmManageTestAppointments";
+            this.Load += new System.EventHandler(this.frmManageTestAppointments_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTestAppoiments)).EndInit();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Applications.LocalDrivingApplications.Controllers.ctrlLocalDrivingLicenseApplicationInfo ctrlLocalDrivingLicenseApplicationInfo1;
+        private System.Windows.Forms.Label lblMainTitle;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dgvTestAppoiments;
+        private System.Windows.Forms.Button btnAddAppointment;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem editTestAppointmentToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem takeTestToolStripMenuItem;
+    }
+}
---
+++ DVLD_UI/Tests/TestAppointments/frmManageTestAppointments.cs
@@ -0,0 +1,103 @@
+﻿using DVLD_BusinessLogic;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DVLD_UI.Tests.TestAppointments
+{
+    public partial class frmManageTestAppointments : Form
+    {
+        private int _LocalDrivingLicenseApplicationID;
+        private DataTable _dtTestAppointments;
+        private clsTestType.enTestType _TestType;
+        public int LocalDrivingLicenseApplicationID { get { return _LocalDrivingLicenseApplicationID; } }
+        public clsTestType.enTestType TestType { get { return _TestType; }
+            set { _TestType = value;
+                switch(value)
+                {
+                    case clsTestType.enTestType.Vision:
+                        lblMainTitle.Text = "Visoin Test Appointments";
+                        break;
+                    case clsTestType.enTestType.Written:
+                        lblMainTitle.Text = "Written Test Appointments";
+                        break;
+                    case clsTestType.enTestType.Street:
+                        lblMainTitle.Text = "Street Test Appointments";
+                        break;
+                }
+            }
+        }
+        public frmManageTestAppointments(int localDrivingLicenseApplicationID, clsTestType.enTestType testType)
+        {
+            InitializeComponent();
+            _LocalDrivingLicenseApplicationID = localDrivingLicenseApplicationID;
+            this.TestType = testType;
+        }
+        void _LoadTestAppointments()
+        {
+            if (clsLocalDrivingLicenseApplication.DoestPersonPassTheTest(LocalDrivingLicenseApplicationID, (int)TestType))
+            {
+                btnAddAppointment.Enabled = false;
+                editTestAppointmentToolStripMenuItem.Enabled = false;
+                takeTestToolStripMenuItem.Enabled = false;
+            }
+            _dtTestAppointments = clsTestAppointment.GetAllTestAppointmentsPerTestType(LocalDrivingLicenseApplicationID, TestType);
+            // TODO the Alignments
+            dgvTestAppoiments.DataSource = _dtTestAppointments;
+            dgvTestAppoiments.Columns[0].Width = 100;
+            dgvTestAppoiments.Columns[1].Width = 250;
+            dgvTestAppoiments.Columns[2].Width = 250;
+            dgvTestAppoiments.Columns[3].Width = 150;
+        }
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        private void frmManageTestAppointments_Load(object sender, EventArgs e)
+        {
+            _LoadTestAppointments();
+            ctrlLocalDrivingLicenseApplicationInfo1.LoadDLAInformation(_LocalDrivingLicenseApplicationID);
+
+        }
+        private void btnAddAppointment_Click(object sender, EventArgs e)
+        {
+            if (clsLocalDrivingLicenseApplication.DoestPersonPassTheTest(LocalDrivingLicenseApplicationID, (int)TestType)) 
+            {
+                MessageBox.Show("This Person Have Already passed this test you cannot add new appointment","Error",MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            frmScheduleTest frm = new frmScheduleTest(LocalDrivingLicenseApplicationID,TestType);
+            frm.ShowDialog();
+            _LoadTestAppointments();
+        }
+        private void editTestAppointmentToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (clsTestAppointment.IsAppointmentsLocked((int)dgvTestAppoiments.CurrentRow.Cells[0].Value))
+            {
+                MessageBox.Show("This Person Have Already take this test you cannot add new appointment", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            frmScheduleTest frm = new frmScheduleTest(LocalDrivingLicenseApplicationID, TestType, (int)dgvTestAppoiments.CurrentRow.Cells[0].Value);
+            frm.ShowDialog();
+            _LoadTestAppointments();
+        }
+
+        private void takeTestToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (clsTestAppointment.IsAppointmentsLocked((int)dgvTestAppoiments.CurrentRow.Cells[0].Value))
+            {
+                MessageBox.Show("This Person Have Already take this test you cannot add new appointment", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            frmScheduledTest frm = new frmScheduledTest(LocalDrivingLicenseApplicationID,  (int)dgvTestAppoiments.CurrentRow.Cells[0].Value , TestType);
+            frm.ShowDialog();
+            _LoadTestAppointments();
+        }
+    }
+}

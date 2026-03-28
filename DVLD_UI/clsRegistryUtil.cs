5bf20c2ca5509c6cf4ee93c0c609662a83ec83a0
Mahmoud Bakir <mahmoodbaker100@gmail.com>
2026-03-28 14:49:52 +0530

    Updating the saving username and key of user to registry instead of files

---
+++ DVLD_UI/clsRegistryUtil.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Win32;
+
+public static class clsRegistryUtil
+{
+    private static readonly string keyPath = @"Software\DVLDApp";
+
+    public static void SaveLogin(string username, string password)
+    {
+        RegistryKey key = Registry.CurrentUser.CreateSubKey(keyPath);
+
+        key.SetValue("UserName", username);
+        key.SetValue("Password", password); // plain text
+
+        key.Close();
+    }
+
+    public static bool LoadLogin(ref string username, ref string password)
+    {
+        RegistryKey key = Registry.CurrentUser.OpenSubKey(keyPath);
+
+        if (key == null)
+            return false;
+
+        object userObj = key.GetValue("UserName");
+        object passObj = key.GetValue("Password");
+
+        if (userObj == null || passObj == null)
+            return false;
+
+        username = userObj.ToString();
+        password = passObj.ToString();
+
+        key.Close();
+        return true;
+    }
+
+    public static void ClearLogin()
+    {
+        try
+        {
+            Registry.CurrentUser.DeleteSubKey(keyPath, false);
+        }
+        catch
+        {
+            throw;
+        }
+    }
+}
--- DVLD_UI/frmLoginScreen.cs
+++ DVLD_UI/frmLoginScreen.cs
@@ -1,98 +1,94 @@
 ﻿using DVLD_BusinessLogic;
-using DVLD_UI.Util;
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Diagnostics;
-using System.Drawing;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace DVLD_UI
 {
     public partial class frmLoginScreen : Form
     {
-        public string filePath = string.Empty;
-        private string _UserName;
-        private string _Password;
+        private string _UserName = string.Empty;
+        private string _Password = string.Empty;
+
         public string UserName
         {
             get { return _UserName; }
         }
+
         public string Password
         {
             get { return _Password; }
         }
-        private clsUser _User;
+
         public frmLoginScreen()
         {
             InitializeComponent();
         }
-        bool isSaved(string filepath)
-        {
-            return clsUtil.GetInfoFromFile(ref _UserName, ref _Password, filepath);
-        }
+
         private void btnLogin_Click(object sender, EventArgs e)
         {
-            clsGlobalUser.gUser = clsUser.GetUserByUserNameAndPassword(txtUserName.Text.Trim(), txtPassword.Text.Trim());
+            clsGlobalUser.gUser = clsUser.GetUserByUserNameAndPassword(
+                txtUserName.Text.Trim(),
+                txtPassword.Text.Trim());
 
-            
             if (clsGlobalUser.gUser != null)
             {
                 if (clsGlobalUser.gUser.IsActive)
                 {
                     if (cbRememberMe.Checked)
                     {
-                        clsUtil.SaveInfoInFile(txtUserName.Text.Trim(),
-                        txtPassword.Text.Trim(),
-                        filePath);
+                        clsRegistryUtil.SaveLogin(
+                            txtUserName.Text.Trim(),
+                            txtPassword.Text.Trim());
                     }
                     else
                     {
-                        clsUtil.ClearFile(filePath);
+                        clsRegistryUtil.ClearLogin();
                     }
-                     
+
                     this.Hide();
                     frmMainForm frm = new frmMainForm(this);
                     frm.ShowDialog();
-
                     return;
                 }
                 else
                 {
-                    MessageBox.Show("Please Contact Your Adming this User is not Active", "UnActive User", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                    lblError.Text = "User Is Not Active!";
+                    MessageBox.Show(
+                        "Please contact admin, this user is not active",
+                        "Inactive User",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Error);
+
+                    lblError.Text = "User is not active!";
                     return;
                 }
             }
             else
             {
-                txtPassword.Text = string.Empty;
-                txtUserName.Text = string.Empty;
-                MessageBox.Show("UserName or Password Are Incorrect", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                lblError.Text = "UserName / Password is wrong!";
-                return;
+                txtPassword.Text = "";
+                txtUserName.Text = "";
+
+                MessageBox.Show(
+                    "Username or Password is incorrect",
+                    "Error",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+
+                lblError.Text = "Username / Password is wrong!";
             }
         }
 
         private void frmLoginScreen_Load(object sender, EventArgs e)
         {
-            // Change the direction of the file so it does not break
-            filePath = "C:\\Users\\mahmo\\dir\\DVLD\\DVLD_UI\\Logs.txt";
-            if (isSaved(filePath))
+            if (clsRegistryUtil.LoadLogin(ref _UserName, ref _Password))
             {
-                txtUserName.Text = UserName.Trim();
-                txtPassword.Text = Password.Trim();
+                txtUserName.Text = _UserName;
+                txtPassword.Text = _Password;
                 cbRememberMe.Checked = true;
             }
             else
             {
-                txtUserName.Text = string.Empty;
-                txtPassword.Text = string.Empty;
+                txtUserName.Text = "";
+                txtPassword.Text = "";
                 cbRememberMe.Checked = false;
             }
         }
